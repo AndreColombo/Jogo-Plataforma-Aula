6d5f7daf616c7038b90fe1483b8cce0c703355d9
AndreColombo <rm15019@fiap.com.br>
2024-09-09 12:49:25 -0300

    2D Game with Idle & Running Animation

---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+
+    private float horizontal;
+    private Rigidbody2D rb;
+    private bool isFacingRight = true;
+    private Animator animator;
+
+    void Awake()
+    {
+        this.rb = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        this.rb.velocity = new Vector2(horizontal * 8f, this.rb.velocity.y);
+
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.rb.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+        }
+
+        Flip();
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
